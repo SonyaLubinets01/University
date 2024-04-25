53fe09cd5aa920986b83c068864ebd0900dbb976
SonyaLubinets01 <167542394+SonyaLubinets01@users.noreply.github.com>
2024-04-25 15:56:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+delegate double CalcDelegate(double num1, double num2, char op);
+
+class CalcProgram
+{
+    static double Calc(double num1, double num2, char op)
+    {
+        switch (op)
+        {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Error: Division by zero.");
+                    return 0;
+                }
+                else
+                {
+                    return num1 / num2;
+                }
+            default:
+                Console.WriteLine("Error: Invalid operator.");
+                return 0;
+        }
+    }
+
+    public static CalcDelegate funcCalc = new CalcDelegate(Calc);
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double num1 = 20;
+        double num2 = 4;
+
+        Console.WriteLine($"{num1} + {num2} = {CalcProgram.funcCalc(num1, num2, '+')}");
+
+        Console.WriteLine($"{num1} - {num2} = {CalcProgram.funcCalc(num1, num2, '-')}");
+
+        Console.WriteLine($"{num1} * {num2} = {CalcProgram.funcCalc(num1, num2, '*')}");
+
+        Console.WriteLine($"{num1} / {num2} = {CalcProgram.funcCalc(num1, num2, '/')}");
+
+        Console.WriteLine($"{num1} / 0 = {CalcProgram.funcCalc(num1, 0, '/')}");
+    }
+}
