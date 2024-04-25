e09eba20c19b315f752e4382cdd9cf111a35fb00
SonyaLubinets01 <167542394+SonyaLubinets01@users.noreply.github.com>
2024-04-25 17:47:31 +0300

    Add files via upload

---
+++ Task 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Product
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string Category { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var products = new List<Product>();
+        products.Add(new Product { Name = "SmartTV", Price = 400, Category = "Electronics" });
+        products.Add(new Product { Name = "Lenovo ThinkPad", Price = 1000, Category = "Electronics" });
+        products.Add(new Product { Name = "Iphone", Price = 700, Category = "Electronics" });
+        products.Add(new Product { Name = "Orange", Price = 2, Category = "Fruits" });
+        products.Add(new Product { Name = "Banana", Price = 3, Category = "Fruits" });
+        ILookup<string, Product> lookup = products.ToLookup(prod => prod.Category);
+        TotalPrice(lookup);
+        Console.ReadKey();
+    }
+
+    static void TotalPrice(ILookup<string, Product> lookup)
+    {
+        foreach (var group in lookup)
+        {
+            double totalPriceForCategory = group.Sum(prod => prod.Price);
+            Console.WriteLine($"{group.Key} Total Price: {totalPriceForCategory}");
+            foreach (var product in group)
+            {
+                Console.WriteLine($"{product.Name} {product.Price}");
+            }
+        }
+    }
+}
